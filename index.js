f5e7cc9c45be4c7c4bf31ab81453dadf3bd1bb5e
Ratul3640 <nahanurislam71@gmail.com>
2025-02-20 17:11:23 +0600

    chart data order-stats added

--- index.js
+++ index.js
@@ -159,7 +159,7 @@ async function run() {
         app.get('/menu/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
-            
+
             const result = await menuCollection.findOne(query);
             res.send(result)
         })
@@ -230,7 +230,8 @@ async function run() {
             const paymentResult = await paymentCollection.insertOne(payment);
 
             console.log('payment info', payment);
-            const query = {_id: {
+            const query = {
+                _id: {
                     $in: payment.cartIds.map(id => new ObjectId(id))
                 }
             };
@@ -238,6 +239,73 @@ async function run() {
             const deleteResult = await cartCollection.deleteMany(query)
             res.send({ paymentResult, deleteResult });
         })
+
+
+        // stata or analytics
+        app.get('/admin-stats', verifyToken, verifyAdmin, async (req, res) => {
+            const users = await userCollection.estimatedDocumentCount();
+            const menuItems = await menuCollection.estimatedDocumentCount();
+            const orders = await paymentCollection.estimatedDocumentCount();
+
+            // this is not the best way
+            // const payments = await paymentCollection.find().toArray();
+            // const revenue = payments.reduce((total, payment) => total + payment.price, 0)
+            const result = await paymentCollection.aggregate([
+                {
+                    $group: {
+                        _id: null,
+                        totalRevenue: {
+                            $sum: '$price'
+                        }
+                    }
+                }
+            ]).toArray()
+
+            const revenue = result.length > 0 ? result[0].totalRevenue : 0;
+            res.send({
+                users,
+                menuItems,
+                orders,
+                revenue
+            })
+        })
+        // order status
+        app.get('/order-stats',verifyToken,verifyAdmin, async (req, res) => {
+            const result = await paymentCollection.aggregate([
+
+                {
+                    $unwind: '$menuItemIds'
+                },
+                {
+                    $lookup: {
+                        from: 'menu',
+                        localField: 'menuItemIds',
+                        foreignField: '_id',
+                        as: 'menuItems'
+                    }
+                },
+                {
+                    $unwind: '$menuItems'
+                },
+                {
+                    $group: {
+                        _id: '$menuItems.category',
+                        quantity: { $sum: 1 },
+                        revenue: { $sum: '$menuItems.price' }
+                    }
+                },
+                {
+                    $project: {
+                        _id: 0,
+                        category: "$_id",
+                        quantity: '$quantity',
+                        revenue: '$revenue'
+                    }
+                }
+            ]).toArray();
+            res.send(result)
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
